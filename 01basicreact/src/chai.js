ebd2eac6575b510b59e402b4057fe5e7300fa8eb
Sharnagat <anupam.sharnagat@ing.com>
2023-10-29 13:31:46 +0100

    initial hellow world

---
+++ 01basicreact/src/chai.js
@@ -0,0 +1,9 @@
+function Chai ()
+{
+    return(
+
+        <h3>Chai from basic react</h3>
+    )
+}
+
+export default Chai
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,9 @@
+function Chai(){
+
+    return(
+
+        <h1>Return from chai js</h1>
+    )
+}
+
+export default Chai;
